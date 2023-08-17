bb6c5ee4b70e38943c045c23dbec91813c76e409
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-17 20:43:27 +0300

    change app context class name  to ApplicationDbContext

--- Crud3/Models/ApplicationDbContext.cs
+++ Crud3/Models/ApplicationDbContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookReviewing_MVC.Models;
 
-public partial class BookReviewingApiContext : DbContext
+public partial class ApplicationDbContext : DbContext
 {
-    public BookReviewingApiContext()
+    public ApplicationDbContext()
     {
     }
 
-    public BookReviewingApiContext(DbContextOptions<BookReviewingApiContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
@@ -42,7 +42,6 @@ public partial class BookReviewingApiContext : DbContext
     public virtual DbSet<Reviewer> Reviewers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=DESKTOP-IG5PLK5;Initial Catalog=BookReviewingAPI;Integrated Security=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
