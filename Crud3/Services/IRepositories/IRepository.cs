d1eab181be5b917005f0febb1d06685a2f336ea9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-17 20:49:13 +0300

    create generics IRepository

---
+++ Crud3/Services/IRepositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace BookReviewing_MVC.Services.IRepositories
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter , bool tracked = true);
+        T Get(bool tracked = true);
+        void Create(T entity);
+        void Delete(T entity);
+    }
+}
