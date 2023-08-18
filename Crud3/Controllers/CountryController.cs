574a15f34e64e4707bf9c77b929b9d5ff0c324ec
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-19 01:52:25 +0300

    create CountryController

---
+++ Crud3/Controllers/CountryController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookReviewing_MVC.Services.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookReviewing_MVC.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private IMapper _mapper;
+
+        public CountryController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
