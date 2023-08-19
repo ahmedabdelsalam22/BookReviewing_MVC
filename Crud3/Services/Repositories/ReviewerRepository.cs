b26cb6d31a5b96a1dfa6995b9c2a2ba32947ea6e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-20 00:53:35 +0300

    create IReviewerRepository and ReviewerRepository

---
+++ Crud3/Services/Repositories/ReviewerRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookReviewing_MVC.Data;
+using BookReviewing_MVC.Services.IRepositories;
+using BookReviewingMVC.Models;
+
+namespace BookReviewing_MVC.Services.Repositories
+{
+    public class ReviewerRepository : Repository<Reviewer>, IReviewerRepository
+    {
+        public ReviewerRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
