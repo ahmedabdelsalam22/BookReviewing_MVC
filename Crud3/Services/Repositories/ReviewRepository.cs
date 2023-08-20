3dff24ca45173f849bfe3424fb0ba5538b35a931
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-20 21:41:07 +0300

    create ReviewRepository

---
+++ Crud3/Services/Repositories/ReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookReviewing_MVC.Data;
+using BookReviewing_MVC.Services.IRepositories;
+using BookReviewingMVC.Models;
+
+namespace BookReviewing_MVC.Services.Repositories
+{
+    public class ReviewRepository : Repository<Review>, IReviewRepository
+    {
+        public ReviewRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
