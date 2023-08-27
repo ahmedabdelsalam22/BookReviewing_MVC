26cb723a0d68c8281a4306f687c2ffbc60b5f7a6
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-28 00:54:08 +0300

    add .Net Identity with all needed configurations

--- Crud3/Data/ApplicationDbContext.cs
+++ Crud3/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using BookReviewingMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookReviewing_MVC.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
